1230b974a207562f1bc3b2b34aac8f71807b2b92
naayoung <ask5046a@gmail.com>
2023-11-06 01:16:42 +0900

    feat: add overlappedID and addMember

--- WebOmokProject/src/main/java/Member/MemberDAO.java
+++ WebOmokProject/src/main/java/Member/MemberDAO.java
@@ -2,7 +2,10 @@ package Member;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 public class MemberDAO {
@@ -12,7 +15,9 @@ public class MemberDAO {
 	
 	public MemberDAO() {
 		try {
-			
+			Context ctx = new InitialContext();
+			Context envContext = (Context)ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource)envContext.lookup("jdbc/oracle");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -22,7 +27,20 @@ public class MemberDAO {
 	public boolean overlappedID(String id) {
 		boolean result = false;
 		try {
-
+			con = dataFactory.getConnection();
+			String query = "SELECT count(*) AS cnt FROM MEMBER_TR WHERE id = ?";
+			pstmt = con.prepareStatement(query);
+			
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next(); //결과 행이 하나 이상 있는 경우, 첫 번째 행으로 이동
+			
+			if (rs.getInt("cnt") > 0) { //rs의 열 값이 0보다 크다면
+				result = true;
+			} else {
+				result = false;
+			}
+			pstmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -32,7 +50,12 @@ public class MemberDAO {
 	//사용자 추가
 	public void addMember(MemberVO memberVO) {
 		try {
-
+			con = dataFactory.getConnection();
+			String query = "INSERT INTO MEMBER_TR (,,,,) VALUES(?,?,?,?)";
+			pstmt = con.prepareStatement(query);
+			
+			pstmt.executeUpdate();  //데이터베이스에 해당 SQL 쿼리가 실행
+			pstmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,11 +64,19 @@ public class MemberDAO {
 	//사용자 존재 여부
 	public boolean isExisted(MemberVO memberVO) {
 		boolean result = false;
-		String id = memberVO.getId();
-		String pwd = memberVO.getPwd();
-		
-		try {
 
+		try {
+			con = dataFactory.getConnection();
+			String query = "";
+			pstmt = con.prepareStatement(query);
+			
+			pstmt.setString(1, memberVO.getId());
+			pstmt.setString(2, memberVO.getPwd());
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			
+			result = Boolean.parseBoolean(rs.getString("result")); //문자열을 불리언으로 변환하는 정적 메서드
+			pstmt.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
