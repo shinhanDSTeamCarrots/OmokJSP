bab97cc9c0a96ca688c2bcf2b0d1084dc8f62ca7
junogogo1234567 <146175531+junogogo1234567@users.noreply.github.com>
2023-11-05 23:32:27 +0900

    Add files via upload

---
+++ WebOmokProject/src/main/java/Room/RoomServlet.java
@@ -0,0 +1,36 @@
+package omok;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/RoomServlet")
+public class RoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public RoomServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+	}
+
+}
