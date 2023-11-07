63f2695f713d70f440e1b89c58ccc3e2457fae53
leelife <leelife@gmail.com>
2023-11-07 18:01:37 +0900

    dsa

--- WebOmokProject/src/main/java/Game/Board.java
+++ WebOmokProject/src/main/java/Game/Board.java
@@ -1,7 +1,7 @@
 package Game;
 
 public class Board {
-	private String[][] map;
+	private static String[][] map;
 	private static final int SIZE = 19;
     public String currentPlayer;
 	
@@ -14,10 +14,6 @@ public class Board {
 		            }
 		        }
 	    }
-	 
-	 public String[][] getMap() {
-		    return map;
-		}
 
 	 
 	 public void print() {
@@ -62,7 +58,169 @@ public class Board {
 	 }
 	 
 	
-	
+		public static boolean checkOmok(int x, int y, String player) {
+		    // 오목을 이루는데 필요한 돌의 수
+		    final int OMOK_COUNT = 5;
+
+		    // 가로, 세로, 대각선 방향의 연속된 돌을 세는데 사용될 카운터
+		    int count;
+
+		    // 가로 방향 체크
+		    count = 1;
+		    // 왼쪽 방향
+		    for (int i = x - 1; i >= 0 && map[y][i].equals(player); i--) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+		    // 오른쪽 방향
+		    for (int i = x + 1; i < map[y].length && map[y][i].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+
+		    // 수직 방향 체크
+		    count = 1;
+		    // 위쪽 방향
+		    for (int i = y - 1; i >= 0 && map[i][x].equals(player); i--) {
+		        count++;
+		    }
+		    // 아래쪽 방향
+		    for (int i = y + 1; i < map.length && map[i][x].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+
+		    // 대각선 (좌상향) 방향 체크
+		    count = 1;
+		    for (int i = 1; x - i >= 0 && y - i >= 0 && map[y - i][x - i].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+		    // 대각선 (우하향) 방향 체크
+		    for (int i = 1; x + i < map[0].length && y + i < map.length && map[y + i][x + i].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+
+		    // 대각선 (우상향) 방향 체크
+		    count = 1;
+		    for (int i = 1; x + i < map[0].length && y - i >= 0 && map[y - i][x + i].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+		    // 대각선 (좌하향) 방향 체크
+		    for (int i = 1; x - i >= 0 && y + i < map.length && map[y + i][x - i].equals(player); i++) {
+		        count++;
+		        if (count == OMOK_COUNT) return true;
+		    }
+
+		    // 모든 방향에 대해 오목이 아니면 false 반환
+		    return false;
+		}
+		
+		 public static boolean checkSix(int x, int y, String player) {
+			    // 육목을 이루는데 필요한 돌의 수
+			    final int SIX_COUNT = 6;
+
+			    // 가로, 세로, 대각선 방향의 연속된 돌을 세는데 사용될 카운터
+			    int count;
+
+			    // 수평 방향 체크 (왼쪽 + 오른쪽)
+			    count = 1;
+			    for (int i = x - 1; i >= 0 && map[y][i].equals(player); i--) count++;
+			    for (int i = x + 1; i < map[0].length && map[y][i].equals(player); i++) count++;
+			    if (count == SIX_COUNT) return true;
+
+			    // 수직 방향 체크 (위 + 아래)
+			    count = 1;
+			    for (int i = y - 1; i >= 0 && map[i][x].equals(player); i--) count++;
+			    for (int i = y + 1; i < map.length && map[i][x].equals(player); i++) count++;
+			    if (count == SIX_COUNT) return true;
+
+			    // 대각선 (좌상향 + 우하향) 체크
+			    count = 1;
+			    for (int i = 1; x - i >= 0 && y - i >= 0 && map[y - i][x - i].equals(player); i++) count++;
+			    for (int i = 1; x + i < map[0].length && y + i < map.length && map[y + i][x + i].equals(player); i++) count++;
+			    if (count == SIX_COUNT) return true;
+
+			    // 대각선 (우상향 + 좌하향) 체크
+			    count = 1;
+			    for (int i = 1; x + i < map[0].length && y - i >= 0 && map[y - i][x + i].equals(player); i++) count++;
+			    for (int i = 1; x - i >= 0 && y + i < map.length && map[y + i][x - i].equals(player); i++) count++;
+			    if (count == SIX_COUNT) return true;
+
+			    // 모든 방향에 대해 육목이 아니면 false 반환
+			    return false;
+			}
+		 	
+		 
+
+		 
+
+
+		  public static boolean checkThree(int x, int y, String player, String[][] board, int directionX, int directionY) {
+			    int[][] directions = {{directionX, directionY}, {-directionX, -directionY}};
+
+			    int stones = 0;
+			    boolean openStart = false, openEnd = false;
+
+			    for (int dir = 0; dir < 2; dir++) {
+			        int dx = directions[dir][0];
+			        int dy = directions[dir][1];
+			        stones = 0;
+			        boolean open = false;
+
+			        for (int i = (dir == 0 ? 0 : 1); i <= 3; i++) {
+			            int nx = x + dx * i;
+			            int ny = y + dy * i;
+
+			            if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length) break;
+
+			            if ("".equals(map[ny][nx]) || map[ny][nx] == null) {
+			                if (i == 0 || i == 3) open = true;
+			                if (i > 0) break; // 돌 시퀀스 이후 열린 공간이 있어야 하므로
+			            } else if (map[ny][nx].equals(player)) {
+			                stones++;
+			            } else {
+			                break;
+			            }
+			        }
+
+			        if (dir == 0 && open) openStart = true; // 첫 번째 방향에서 열린 끝을 확인
+			        if (dir == 1 && open) openEnd = true; // 두 번째 방향에서 열린 끝을 확인
+			    }
+
+			    return stones == 3 && openStart && openEnd;
+			}
+
+
+			public static boolean checkDoubleThree(int x, int y, String player) {
+			    // 삼이 형성되는지 여부를 방향별로 체크하고 삼의 개수를 카운트
+			    int threes = 0;
+
+			    // 수평 방향으로 삼을 확인
+			    if (checkThree(x, y, player, map, 1, 0)) {
+			        threes++;
+			    }
+
+			    // 수직 방향으로 삼을 확인
+			    if (checkThree(x, y, player, map, 0, 1)) {
+			        threes++;
+			    }
+
+			    // 대각선 (좌상향 + 우하향) 방향으로 삼을 확인
+			    if (checkThree(x, y, player, map, 1, 1)) {
+			        threes++;
+			    }
+
+			    // 대각선 (우상향 + 좌하향) 방향으로 삼을 확인
+			    if (checkThree(x, y, player, map, 1, -1)) {
+			        threes++;
+			    }
+
+			    // 삼이 두 개 이상이면 쌍삼으로 간주
+			    return threes >= 2;
+			}
 
 	 
 }
--- WebOmokProject/src/main/java/Game/Omok.java
+++ WebOmokProject/src/main/java/Game/Omok.java
@@ -2,7 +2,9 @@ package Game;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,65 +14,69 @@ import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 
+import Rank.LogVO;
+import Rank.RankDAO;
+
 
 @WebServlet("/omok")
 public class Omok extends HttpServlet {
 	// 나중에 ajax 양방향 통신을 위해 json.simpple-1.1.1.jar 라이브러리 가져올 것
 	
-	String dbURL = "jdbc:oracle:thin:@hostname:port:sid"; // 호스트명, 포트, SID에 해당하는 부분은 나중에 변경해야 함
-	String dbUser = "username"; // DB 사용자 이름
-	String dbPass = "password"; // DB 비밀번호
-	
+	RankDAO rankDao;
+	private static int win_count = 0;
+	private static int lose_count = 0;
 	@Override
 	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    // 결과를 파라미터에서 가져옴 (예: result= 승 or result= 패배)
-	    String result = request.getParameter("result");
-
-	    // 세션에서 사용자 정보를 가져옴
-	    HttpSession session = request.getSession();
-	    String username = (String) session.getAttribute("username"); 
-	    
-	    // DAO를 사용하여 결과를 DB에 저장
-	    
-	    // Dao dao = new Dao(dbURL, dbUser, dbPass);
-	    boolean updateSuccess = dao.updateGameResult(username, result);
-	    
-	    // JSON 객체로 결과 생성
-	    JSONObject jsonResponse = new JSONObject();
-	    if (updateSuccess) {
-	        jsonResponse.put("message", "결과가 성공적으로 업데이트 되었습니다.");
-	    } else {
-	        jsonResponse.put("message", "결과 업데이트에 실패하였습니다.");
-	    }
-	    
-	    // JSON 응답 전송
-	    response.setContentType("application/json");
-	    response.setCharacterEncoding("UTF-8");
-	    PrintWriter out = response.getWriter();
-	    out.print(jsonResponse.toString());
-	    out.flush();
+	   ServletContext sc = this.getServletContext();
+	   
+	   String WinCount = request.getParameter("Win");
+	   if (WinCount != null) {
+		   win_count++;
+	   }
+	   
+	   String LoseCount = request.getParameter("Lose");
+	   if (LoseCount != null) {
+		   lose_count++;
+	   }
+	   
+	   if(rankDao == null)
+		   rankDao = new RankDAO();
+	   try {
+		   LogVO logvo = new LogVO();
+		   logvo.setWinner_no(win_count);
+		   logvo.setLoser_no(lose_count);
+		   rankDao.addLog(logvo);
+		   
+	   } catch (Exception e) {
+		   
+	   }
 	}
 
 	
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 GameLogic gamelogic = new GameLogic();
+		
 		 
 		 // JSP로 부터 x값을 가져옴. JSP에 <input type="text" name="id"> 있었다면
 		 // 서블릿에서 request.getParameter("y_input") 와 같은 방식으로, 클라이언트가 입력한 ID가 뭐였는지 알 수 있다
-		 int x = Integer.parseInt(request.getParameter("x_input")); 
-		 int y = Integer.parseInt(request.getParameter("y_input")); 
+		 int x = Integer.parseInt(request.getParameter("x_row")); 
+		 int y = Integer.parseInt(request.getParameter("y_col")); 
 		 
-		 // x,y와 똑같은 방식으로 가져옴
-	     String black = request.getParameter("black_player");
-	     String white = request.getParameter("white_player");
+		 // 
+	     String WhoIsPlayer = request.getParameter("Gameplayer");
 	     
 	     // 여러 요청에 걸쳐 게임 상태를 서버에 저장하고 검색하기 위해 HttpSession을 사용 (상태유지 행위).
 		 HttpSession session = request.getSession();	
 		 
-		 // 게임 상태를 유지하기 위해 Board 객체가 세션에 저장됨
-		 Board board = (Board) session.getAttribute("board");  
+		 session.setAttribute("sessionPlayer", WhoIsPlayer);
+		 
+		 // 서로 다른 플레이어(클라이언트)끼리 보드판을 공유하고자하여, 서블릿컨텍스트로 저장한다.
+		
+		 ServletContext context = getServletContext();
+		 
+		 // Board 객체를 생성.. 하지만 하지만 board에 아무런 데이터가 없다면 if 조건문을 타고 객체를 만든다.. 다음 실행때는 if문 안타고 getAttibute 호출해서 "board" 속성 가져옴..
+		 Board board = (Board) context.getAttribute("board");  
 	        if (board == null) {
 	            board = new Board();
 	            session.setAttribute("board", board);  // 서블릿은 데이터를 JSP에 보내줘야함. 그런데 어떻게??
@@ -79,16 +85,13 @@ public class Omok extends HttpServlet {
 	        }
 	        
 	        // 현재 플레이어
-	        String currentPlayer = board.isBlackTurn(black, white);
+	        // String currentPlayer = board.isBlackTurn(black, white);
 	        
 	        // 검은 플레이어면 이면 "B", 흰 플레이어면 "W"를 사용한다.
-	        String stone = currentPlayer.equals("black_player") ? "B" : "W"; 
+	        String stone = WhoIsPlayer.equals("black_player") ? "B" : "W"; 
 	        
 	        //  이 메소드는 일반적으로 현재 플레이어가 (x, y) 위치에 돌을 놓으려고 시도
 		    boolean success = board.play(x, y, stone); 
-		    
-		    // 현재 오목판 출력
-		    // board.print();
 		    
 		    boolean Omok = false;    // 오목 여부
 		    boolean Six = false;     // 육목 여부
@@ -97,19 +100,19 @@ public class Omok extends HttpServlet {
 
 		    // 돌을 놓은 후의 상태 체크
 		    if (success) {
-		    	Omok = gamelogic.checkOmok(x, y, currentPlayer, board.getMap());
-		    	Six = gamelogic.checkSix(x, y, currentPlayer, board.getMap());
-		    	DoubleThree = gamelogic.checkDoubleThree(x, y, currentPlayer, board.getMap());
+		    	Omok = Board.checkOmok(x, y, WhoIsPlayer);
+		    	Six = Board.checkSix(x, y, WhoIsPlayer);
+		    	DoubleThree = Board.checkDoubleThree(x, y, WhoIsPlayer);
 		        
 		        if (Omok) {
-		            message = currentPlayer + " 승리!";
+		            message = WhoIsPlayer + " 승리!";
 		        } else if (Six) {
-		            message = "육목은 허용되지 않습니다!";
-		            
+		            message = "6X";
+		                                                                                                                           
 		            // 육목이면 성공을 false로 변경
 		            success = false; 
 		        } else if (DoubleThree) {
-		            message = "쌍삼은 허용되지 않습니다!";
+		            message = "3X";
 		            
 		            // 쌍삼이면 성공을 false로 변경
 		            success = false; 
@@ -129,7 +132,7 @@ public class Omok extends HttpServlet {
 		    jsonResponse.put("success", String.valueOf(success));
 		    jsonResponse.put("message", message);
 		    if (success) {
-		        jsonResponse.put("currentPlayer", currentPlayer);
+		        jsonResponse.put("currentPlayer", WhoIsPlayer);
 		    }
 	        
 	        // 응답 타입을 JSON으로 설정
